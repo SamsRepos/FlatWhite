2fd3c93542432114b867b5f32c5ef808839acc9e
samluddite <56632175+SamsRepos@users.noreply.github.com>
2023-11-26 12:30:25 +0000

    added Vec2f unit vector static getters

--- FlatWhite/include/common/Vec2f.h
+++ FlatWhite/include/common/Vec2f.h
@@ -5,25 +5,31 @@
 
 namespace fw {
 
-	class Vec2f : public sf::Vector2f
-	{
-	public:
-		Vec2f();
-		Vec2f(float xAndY);
-		Vec2f(float x, float y);
-		//Vec2f(sf::Vector2f& sfVector2f);
-		Vec2f(sf::Vector2<float> sfVector2f);
-		//Vec2f(b2Vec2& b2Vec);
-		Vec2f(b2Vec2 b2Vec);
-		operator b2Vec2() const;
+class Vec2f : public sf::Vector2f
+{
+public:
+	Vec2f();
+	Vec2f(float xAndY);
+	Vec2f(float x, float y);
+	//Vec2f(sf::Vector2f& sfVector2f);
+	Vec2f(sf::Vector2<float> sfVector2f);
+	//Vec2f(b2Vec2& b2Vec);
+	Vec2f(b2Vec2 b2Vec);
+	operator b2Vec2() const;
 
-		float magnitude() const;
-		float magnitudeSquared() const;
+	float magnitude() const;
+	float magnitudeSquared() const;
 
-		void normalise();
-		Vec2f normalised() const;
+	void normalise();
+	Vec2f normalised() const;
 
-		bool isZero() const;
-	};
+	bool isZero() const;
+
+	inline static Vec2f zero() { return Vec2f(0.f); };
+	inline static Vec2f unitLeft() { return Vec2f(-1.f, 0.f); };
+	inline static Vec2f unitRight() { return Vec2f(1.f, 0.f); };
+	inline static Vec2f unitUp() { return Vec2f(0.f, -1.f); };
+	inline static Vec2f unitDown() { return Vec2f(0.f, 1.f); };
+};
 
 }
--- FlatWhite/src/common/Vec2f.cpp
+++ FlatWhite/src/common/Vec2f.cpp
@@ -2,92 +2,92 @@
 
 namespace fw {
 
-	Vec2f::Vec2f()
-		:
-		sf::Vector2f()
-	{
+Vec2f::Vec2f()
+	:
+	sf::Vector2f()
+{
 
-	}
+}
 
-	Vec2f::Vec2f(float xAndY)
-		:
-		sf::Vector2f(xAndY, xAndY)
-	{
+Vec2f::Vec2f(float xAndY)
+	:
+	sf::Vector2f(xAndY, xAndY)
+{
 
-	}
+}
 
-	Vec2f::Vec2f(float x_, float y_)
-		:
-		sf::Vector2f(x_, y_)
-	{
+Vec2f::Vec2f(float x_, float y_)
+	:
+	sf::Vector2f(x_, y_)
+{
 
-	}
+}
 
-	//Vec2f::Vec2f(sf::Vector2f& sfVector2f)
-	//	:
-	//	sf::Vector2f(sfVector2f)
-	//{
-	//	
-	//}
+//Vec2f::Vec2f(sf::Vector2f& sfVector2f)
+//	:
+//	sf::Vector2f(sfVector2f)
+//{
+//	
+//}
 
-	Vec2f::Vec2f(sf::Vector2<float> sfVector2f)
-		:
-		sf::Vector2f(sfVector2f)
-	{
+Vec2f::Vec2f(sf::Vector2<float> sfVector2f)
+	:
+	sf::Vector2f(sfVector2f)
+{
 
-	}
+}
 
-	//Vec2f::Vec2f(b2Vec2& b2Vec)
-	//	:
-	//	sf::Vector2f(b2Vec.x, b2Vec.y)
-	//{
-	//
-	//}
+//Vec2f::Vec2f(b2Vec2& b2Vec)
+//	:
+//	sf::Vector2f(b2Vec.x, b2Vec.y)
+//{
+//
+//}
 
-	Vec2f::Vec2f(b2Vec2 b2Vec)
-		:
-		sf::Vector2f(b2Vec.x, b2Vec.y)
-	{
+Vec2f::Vec2f(b2Vec2 b2Vec)
+	:
+	sf::Vector2f(b2Vec.x, b2Vec.y)
+{
 
-	}
+}
 
-	Vec2f::operator b2Vec2() const
-	{
-		return b2Vec2(x, y);
-	}
+Vec2f::operator b2Vec2() const
+{
+	return b2Vec2(x, y);
+}
 
-	float Vec2f::magnitude() const
-	{
-		return sqrtf((x * x) + (y * y));
-	}
+float Vec2f::magnitude() const
+{
+	return sqrtf((x * x) + (y * y));
+}
 
 
-	float Vec2f::magnitudeSquared() const
-	{
-		return (x * x) + (y * y);
-	}
+float Vec2f::magnitudeSquared() const
+{
+	return (x * x) + (y * y);
+}
 
-	void Vec2f::normalise()
-	{
-		if (isZero()) return;
+void Vec2f::normalise()
+{
+	if (isZero()) return;
 
-		float length = magnitude();
-		float recipLength = 1.f / length;
+	float length = magnitude();
+	float recipLength = 1.f / length;
 
-		x *= recipLength;
-		y *= recipLength;
-	}
+	x *= recipLength;
+	y *= recipLength;
+}
 
-	Vec2f Vec2f::normalised() const
-	{
-		Vec2f res(x, y);
-		res.normalise();
-		return res;
-	}
+Vec2f Vec2f::normalised() const
+{
+	Vec2f res(x, y);
+	res.normalise();
+	return res;
+}
 
-	bool Vec2f::isZero() const
-	{
-		return (fabsf(x) < FLT_EPSILON) && (fabsf(y) < FLT_EPSILON);
-	}
+bool Vec2f::isZero() const
+{
+	return (fabsf(x) < FLT_EPSILON) && (fabsf(y) < FLT_EPSILON);
+}
 
 }
