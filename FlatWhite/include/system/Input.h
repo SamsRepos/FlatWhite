a73dfb8423ed6d8652a6fad266e66d66df01c17d
samluddite <56632175+SamsRepos@users.noreply.github.com>
2023-11-27 15:32:09 +0000

    Added xbox controls

--- FlatWhite/include/system/Input.h
+++ FlatWhite/include/system/Input.h
@@ -7,61 +7,98 @@
 namespace fw
 {
 
-	const size_t KEYS_RANGE = 256;
+const size_t KEYS_RANGE = 256;
 
-	class Input
+enum class XboxButton : unsigned int
+{
+	A       = 0,
+	B       = 1,
+	X       = 2,
+	Y       = 3,
+	LB      = 4,
+	RB      = 5,
+	BACK    = 6,
+	START   = 7,
+	L_CLICK = 8,
+	R_CLICK = 9
+};
+const unsigned int XBOX_BUTTONS_RANGE = 10;
+
+enum class XboxTrigger
+{
+	LT,
+	RT
+};
+const float XBOX_TRIGGER_PRESSED_THRESHOLD_DEFAULT = 70.f;
+
+enum class XboxStick
+{
+	Left,
+	Right,
+	DPad
+};
+
+class Input
+{
+public:
+	static bool eventIsInput(const sf::Event& event);
+	void perFrameUpdate();
+	void eventUpdate(const sf::Event& event);
+
+	bool isKeyDown(int key) const;
+	bool isKeyUp(int key) const;
+	bool isKeyPressedNow(int key) const;
+	bool isAnyKeyDown() const;
+
+	int getMousePosX() const;
+	int getMousePosY() const;
+	Vec2f getMousePosition() const;
+	inline bool isMouseInWindow() const;
+
+	bool isMouseLeftDown() const;
+	bool isMouseRightDown() const;
+	bool isMouseLeftClickedNow() const;
+	bool isMouseRightClickedNow() const;
+
+	bool isXboxControllerConnected() const;
+	bool isXboxButtonDown(XboxButton button) const;
+	bool isXboxButtonPressedNow(XboxButton button) const;
+	bool isXboxTriggerDown(XboxTrigger trigger, float threshold = XBOX_TRIGGER_PRESSED_THRESHOLD_DEFAULT) const;
+	Vec2f getXboxStick(XboxStick stick) const;
+
+private:
+	void setKeyDown(int key);
+	void setKeyUp(int key);
+	void updateAnyKeyDown();
+
+	void setMousePosX(int x);
+	void setMousePosY(int y);
+	void setMousePosition(int x, int y);
+	void setMouseInWindow(bool in);
+
+	void setMouseLeftDown(bool down);
+	void setMouseRightDown(bool down);
+
+	//void setMouseWheelDelta();
+
+	bool m_keysDown[KEYS_RANGE]{ false };
+	bool m_keysDownPreviously[KEYS_RANGE]{ false };
+	bool m_keysPressedNow[KEYS_RANGE]{ false };
+	bool m_anyKeyDown = false;
+
+	bool m_xboxButtonsDown[XBOX_BUTTONS_RANGE]{ false };
+	bool m_xboxButtonsDownPreviously[XBOX_BUTTONS_RANGE]{ false };
+	bool m_xboxButtonsPressedNow[XBOX_BUTTONS_RANGE]{ false };
+
+	struct Mouse
 	{
-	public:
-		static bool eventIsInput(const sf::Event& event);
-		void perFrameUpdate();
-		void eventUpdate(const sf::Event& event);
-
-		bool isKeyDown(int key) const;
-		bool isKeyUp(int key) const;
-		bool isKeyPressedNow(int key) const;
-		bool isAnyKeyDown() const;
-
-		int getMousePosX() const;
-		int getMousePosY() const;
-		Vec2f getMousePosition() const;
-		inline bool isMouseInWindow() const;
-
-		bool isMouseLeftDown() const;
-		bool isMouseRightDown() const;
-		bool isMouseLeftClickedNow() const;
-		bool isMouseRightClickedNow() const;
-
-	private:
-		void setKeyDown(int key);
-		void setKeyUp(int key);
-		void updateAnyKeyDown();
-
-		void setMousePosX(int x);
-		void setMousePosY(int y);
-		void setMousePosition(int x, int y);
-		void setMouseInWindow(bool in);
-
-		void setMouseLeftDown(bool down);
-		void setMouseRightDown(bool down);
-
-		void debugOut();
-
-		//void setMouseWheelDelta();
-
-		bool m_keysDown[KEYS_RANGE]{ false };
-		bool m_keysDownPreviously[KEYS_RANGE]{ false };
-		bool m_keysPressedNow[KEYS_RANGE]{ false };
-		bool m_anyKeyDown = false;
-
-		struct Mouse
-		{
-			int x = 0, y = 0;
-			bool left = false, right = false;
-			bool inWindow = false;
-			int wheelDelta = 0;
-		};
-		Mouse m_mouse;
-		Mouse m_previousMouse;
+		int x = 0, y = 0;
+		bool left = false, right = false;
+		bool inWindow = false;
+		int wheelDelta = 0;
 	};
+	Mouse m_mouse;
+	Mouse m_previousMouse;
+};
 
 }
--- FlatWhite/src/system/Input.cpp
+++ FlatWhite/src/system/Input.cpp
@@ -5,233 +5,327 @@
 namespace fw
 {
 
-	//
-	//  PUBLIC UPDATES:
-	//
+const float XBOX_AXIS_MIN_THRESHOLD = 10.f;
 
-	bool Input::eventIsInput(const sf::Event& event)
-	{
-		if (event.type == sf::Event::Closed)
-			switch (event.type) {
-			case sf::Event::KeyPressed:
-			case sf::Event::KeyReleased:
-			case sf::Event::MouseMoved:
-			case sf::Event::MouseButtonPressed:
-			case sf::Event::MouseButtonReleased:
-			case sf::Event::MouseEntered:
-			case sf::Event::MouseLeft:
-				//case sf::Event::MouseWheelScrolled:
-				return true;
-			default:
-				return false;
-			}
-	}
+//
+//  PUBLIC UPDATES:
+//
 
-	void Input::perFrameUpdate()
-	{
-		std::copy(std::begin(m_keysDown), std::end(m_keysDown), std::begin(m_keysDownPreviously));
-		m_previousMouse = m_mouse;
-	}
-
-	void Input::eventUpdate(const sf::Event& event)
-	{
+bool Input::eventIsInput(const sf::Event& event)
+{
+	if (event.type == sf::Event::Closed)
 		switch (event.type) {
 		case sf::Event::KeyPressed:
-			setKeyDown(event.key.code);
-			updateAnyKeyDown();
-			break;
 		case sf::Event::KeyReleased:
-			setKeyUp(event.key.code);
-			updateAnyKeyDown();
-			break;
 		case sf::Event::MouseMoved:
-			setMousePosition(event.mouseMove.x,
-				event.mouseMove.y);
-			break;
 		case sf::Event::MouseButtonPressed:
-			if (event.mouseButton.button == sf::Mouse::Left)
-			{
-				setMouseLeftDown(true);
-			}
-			if (event.mouseButton.button == sf::Mouse::Right)
-			{
-				setMouseRightDown(true);
-			}
-			break;
 		case sf::Event::MouseButtonReleased:
-			if (event.mouseButton.button == sf::Mouse::Left)
-			{
-				setMouseLeftDown(false);
-			}
-			if (event.mouseButton.button == sf::Mouse::Right)
-			{
-				setMouseRightDown(false);
-			}
-			break;
 		case sf::Event::MouseEntered:
-			setMouseInWindow(true);
-			break;
 		case sf::Event::MouseLeft:
-			setMouseInWindow(false);
-			break;
 			//case sf::Event::MouseWheelScrolled:
-			//	event.mouseWheel.delta // pos up, neg down
-			//	break;
+			return true;
+		default:
+			return false;
 		}
+}
 
-		//debugOut();
-	}
+void Input::perFrameUpdate()
+{
+	std::copy(std::begin(m_keysDown), std::end(m_keysDown), std::begin(m_keysDownPreviously));
+	m_previousMouse = m_mouse;
 
-	//
-	//  PUBLIC GETTERS:
-	//
-
-	// returns true if out of range
-	bool keysRangeCheck(int key)
+	if (sf::Joystick::isConnected(0))
 	{
-		return key < 0 || key >= KEYS_RANGE;
-	}
+		std::copy(std::begin(m_xboxButtonsDown), std::end(m_xboxButtonsDown), std::begin(m_xboxButtonsDownPreviously));
 
-	bool Input::isKeyDown(int key) const
-	{
-		if (keysRangeCheck(key)) return false;
-
-		return m_keysDown[key];
-	}
-
-	bool Input::isKeyUp(int key) const
-	{
-		if (keysRangeCheck(key)) return false;
-
-		return !(m_keysDown[key]);
-	}
-
-	bool Input::isKeyPressedNow(int key) const
-	{
-		if (keysRangeCheck(key)) return false;
-
-		bool isKeyDownNow = isKeyDown(key);
-		bool wasKeyUpBefore = !(m_keysDownPreviously[key]);
-
-		bool res = isKeyDownNow && wasKeyUpBefore;
-
-		return res;
-	}
-
-	bool Input::isAnyKeyDown() const
-	{
-		return m_anyKeyDown;
-	}
-
-	int Input::getMousePosX() const
-	{
-		return m_mouse.x;
-	}
-
-	int Input::getMousePosY() const
-	{
-		return m_mouse.y;
-	}
-
-	Vec2f Input::getMousePosition() const
-	{
-		return Vec2f(m_mouse.x, m_mouse.y);
-	}
-
-	bool Input::isMouseInWindow() const
-	{
-		return m_mouse.inWindow;
-	}
-
-	bool Input::isMouseLeftDown() const
-	{
-		return m_mouse.left;
-	}
-
-	bool Input::isMouseRightDown() const
-	{
-		return m_mouse.right;
-	}
-
-	bool Input::isMouseLeftClickedNow() const
-	{
-		return m_mouse.left && !m_previousMouse.left;
-	}
-
-	bool Input::isMouseRightClickedNow() const
-	{
-		return m_mouse.right && !m_previousMouse.right;
-	}
-
-	//
-	//  PRIVATE:
-	//
-
-
-	void Input::setKeyDown(int key)
-	{
-		if (keysRangeCheck(key)) return;
-
-		m_keysDown[key] = true;
-	}
-
-	void Input::setKeyUp(int key)
-	{
-		if (keysRangeCheck(key)) return;
-
-		m_keysDown[key] = false;
-	}
-
-	void Input::updateAnyKeyDown()
-	{
-		for (int i = 0; i < KEYS_RANGE; ++i)
+		sf::Joystick::update();
+		for (auto i = 0; i < XBOX_BUTTONS_RANGE; ++i)
 		{
-			if (m_keysDown[i]) {
-				m_anyKeyDown = true;
-				return;
-			}
+			m_xboxButtonsDown[i] = sf::Joystick::isButtonPressed(0, unsigned int(i));
 		}
-		m_anyKeyDown = false;
+	}
+	
+}
+
+void Input::eventUpdate(const sf::Event& event)
+{
+	switch (event.type) {
+	case sf::Event::KeyPressed:
+		setKeyDown(event.key.code);
+		updateAnyKeyDown();
+		break;
+	case sf::Event::KeyReleased:
+		setKeyUp(event.key.code);
+		updateAnyKeyDown();
+		break;
+	case sf::Event::MouseMoved:
+		setMousePosition(event.mouseMove.x,
+			event.mouseMove.y);
+		break;
+	case sf::Event::MouseButtonPressed:
+		if (event.mouseButton.button == sf::Mouse::Left)
+		{
+			setMouseLeftDown(true);
+		}
+		if (event.mouseButton.button == sf::Mouse::Right)
+		{
+			setMouseRightDown(true);
+		}
+		break;
+	case sf::Event::MouseButtonReleased:
+		if (event.mouseButton.button == sf::Mouse::Left)
+		{
+			setMouseLeftDown(false);
+		}
+		if (event.mouseButton.button == sf::Mouse::Right)
+		{
+			setMouseRightDown(false);
+		}
+		break;
+	case sf::Event::MouseEntered:
+		setMouseInWindow(true);
+		break;
+	case sf::Event::MouseLeft:
+		setMouseInWindow(false);
+		break;
+		//case sf::Event::MouseWheelScrolled:
+		//	event.mouseWheel.delta // pos up, neg down
+		//	break;
 	}
 
+	//debugOut();
+}
 
-	void Input::setMousePosX(int x)
+//
+//  PUBLIC GETTERS:
+//
+
+// returns true if out of range
+bool rangeCheck(int key, int range)
+{
+	return key < 0 || key >= range;
+}
+
+bool Input::isKeyDown(int key) const
+{
+	if (rangeCheck(key, KEYS_RANGE)) return false;
+
+	return m_keysDown[key];
+}
+
+bool Input::isKeyUp(int key) const
+{
+	if (rangeCheck(key, KEYS_RANGE)) return false;
+
+	return !(m_keysDown[key]);
+}
+
+bool Input::isKeyPressedNow(int key) const
+{
+	if (rangeCheck(key, KEYS_RANGE)) return false;
+
+	bool isKeyDownNow = isKeyDown(key);
+	bool wasKeyUpBefore = !(m_keysDownPreviously[key]);
+
+	bool res = isKeyDownNow && wasKeyUpBefore;
+
+	return res;
+}
+
+bool Input::isAnyKeyDown() const
+{
+	return m_anyKeyDown;
+}
+
+int Input::getMousePosX() const
+{
+	return m_mouse.x;
+}
+
+int Input::getMousePosY() const
+{
+	return m_mouse.y;
+}
+
+Vec2f Input::getMousePosition() const
+{
+	return Vec2f(m_mouse.x, m_mouse.y);
+}
+
+bool Input::isMouseInWindow() const
+{
+	return m_mouse.inWindow;
+}
+
+bool Input::isMouseLeftDown() const
+{
+	return m_mouse.left;
+}
+
+bool Input::isMouseRightDown() const
+{
+	return m_mouse.right;
+}
+
+bool Input::isMouseLeftClickedNow() const
+{
+	return m_mouse.left && !m_previousMouse.left;
+}
+
+bool Input::isMouseRightClickedNow() const
+{
+	return m_mouse.right && !m_previousMouse.right;
+}
+
+bool Input::isXboxControllerConnected() const
+{
+	return sf::Joystick::isConnected(0);
+}
+
+bool Input::isXboxButtonDown(XboxButton button) const
+{
+	if (rangeCheck(unsigned int(button), XBOX_BUTTONS_RANGE)) return false;
+
+	return m_xboxButtonsDown[unsigned int(button)];
+}
+
+bool Input::isXboxButtonPressedNow(XboxButton button) const
+{
+	if (rangeCheck(unsigned int(button), XBOX_BUTTONS_RANGE)) return false;
+
+	bool isButtonDownNow   = isXboxButtonDown(button);
+	bool wasButtonUpBefore = !(m_xboxButtonsDownPreviously[unsigned int(button)]);
+
+	bool res = isButtonDownNow && wasButtonUpBefore;
+
+	return res;
+}
+
+
+bool Input::isXboxTriggerDown(XboxTrigger trigger, float threshold) const
+{
+	//Z LT positive, RT negative
+	float z = sf::Joystick::getAxisPosition(0, sf::Joystick::Z);
+
+	switch(trigger)
 	{
-		m_mouse.x = x;
-	}
-
-	void Input::setMousePosY(int y)
+	case XboxTrigger::LT:
 	{
-		m_mouse.y = y;
+		return (z > threshold);
 	}
-
-	void Input::setMousePosition(int x, int y)
+		break;
+	case XboxTrigger::RT:
 	{
-		setMousePosX(x);
-		setMousePosY(y);
+		return (z < -threshold);
 	}
+	break;
+	}
+}
 
-	void Input::setMouseInWindow(bool in)
+Vec2f Input::getXboxStick(XboxStick stick) const
+{
+	auto axesToVector = [&](sf::Joystick::Axis xAxis, sf::Joystick::Axis yAxis)
 	{
-		m_mouse.inWindow = in;
-	}
+		float x = sf::Joystick::getAxisPosition(0, xAxis);
+		if (fabsf(x) < XBOX_AXIS_MIN_THRESHOLD) x = 0.f;
 
+		float y = sf::Joystick::getAxisPosition(0, yAxis);
+		if (fabsf(y) < XBOX_AXIS_MIN_THRESHOLD) y = 0.f;
 
-	void Input::setMouseLeftDown(bool down)
+		return fw::Vec2f(x, y);
+	};
+
+	switch (stick)
 	{
-		m_mouse.left = down;
-	}
-
-	void Input::setMouseRightDown(bool down)
+	case XboxStick::Left:
 	{
-		m_mouse.right = down;
+		return axesToVector(
+			sf::Joystick::Axis::X,
+			sf::Joystick::Axis::Y
+		);
 	}
-
-	void Input::debugOut()
+	break;
+	case XboxStick::Right:
 	{
-		std::cout << "Mouse x: " << m_mouse.x << " ";
-		std::cout << "Mouse y: " << m_mouse.y << " ";
-		std::cout << std::endl;
+		return axesToVector(
+			sf::Joystick::Axis::U,
+			sf::Joystick::Axis::V
+		);
 	}
+	break;
+	case XboxStick::DPad:
+	{
+		return axesToVector(
+			sf::Joystick::Axis::PovX,
+			sf::Joystick::Axis::PovY
+		);
+	}
+	break;
+	}
+}
+
+//
+//  PRIVATE:
+//
+
+
+void Input::setKeyDown(int key)
+{
+	if (rangeCheck(key, KEYS_RANGE)) return;
+
+	m_keysDown[key] = true;
+}
+
+void Input::setKeyUp(int key)
+{
+	if (rangeCheck(key, KEYS_RANGE)) return;
+
+	m_keysDown[key] = false;
+}
+
+void Input::updateAnyKeyDown()
+{
+	for (int i = 0; i < KEYS_RANGE; ++i)
+	{
+		if (m_keysDown[i]) {
+			m_anyKeyDown = true;
+			return;
+		}
+	}
+	m_anyKeyDown = false;
+}
+
+
+void Input::setMousePosX(int x)
+{
+	m_mouse.x = x;
+}
+
+void Input::setMousePosY(int y)
+{
+	m_mouse.y = y;
+}
+
+void Input::setMousePosition(int x, int y)
+{
+	setMousePosX(x);
+	setMousePosY(y);
+}
+
+void Input::setMouseInWindow(bool in)
+{
+	m_mouse.inWindow = in;
+}
+
+
+void Input::setMouseLeftDown(bool down)
+{
+	m_mouse.left = down;
+}
+
+void Input::setMouseRightDown(bool down)
+{
+	m_mouse.right = down;
+}
 
 }
